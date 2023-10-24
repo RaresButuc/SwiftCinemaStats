d1bb32749b3c7898d28ee9a5c6e6e3c25ceea273
RaresButuc <eusuntbutuc01@gmail.com>
2023-10-24 23:59:06 +0300

    New Page Structure of the Stats

--- client/src/components/HomeMovie.js
+++ client/src/components/HomeMovie.js
@@ -1,29 +1,44 @@
 export default function ChosenMovieInformations({ movie }) {
   return (
-    <div className="searchedMovie">
-      <h1>{movie.Title}</h1>
-      <img className="myimg" src={movie.Poster} alt="movie.poster.png" />
-      <br></br>
-      <h3 className="movieCategTag">Release Date:</h3>
-      <h3>{movie.Released}</h3>
-
-      <h3 className="movieCategTag">Directed by:</h3>
-      <h3>{movie.Director}</h3>
-
-      <h3 className="movieCategTag">Screenwriting by:</h3>
-      <h3>{movie.Writer}</h3>
-
-      <h3 className="movieCategTag">Actors:</h3>
-      <h3>{movie.Actors}</h3>
-
-      <h3 className="movieCategTag">Genres: </h3>
-      <h3>{movie.Genre}</h3>
-
-      <h3 className="movieCategTag">Plot:</h3>
-      <h3>{movie.Plot}</h3>
-
-      <h3 className="movieCategTag">Ratings: </h3>
-      <h3>{movie.imdbRating}⭐</h3>
+    <div className="searchedMovieContainer">
+      <div className="posterTitle">
+        <img className="myimg" src={movie.Poster} alt="movie.poster.png" />
+      </div>
+      <h1 className="movieTitle">{movie.Title}</h1>
+      <div className="movieDetails">
+        <div className="movieCategColumn">
+          <div className="movieCategGroup">
+            <h3 className="movieCategTag">Release Date:</h3>
+            <h3>{movie.Released}</h3>
+          </div>
+          <div className="movieCategGroup">
+            <h3 className="movieCategTag">Directed by:</h3>
+            <h3>{movie.Director}</h3>
+          </div>
+          <div className="movieCategGroup">
+            <h3 className="movieCategTag">Screenwriting by:</h3>
+            <h3>{movie.Writer}</h3>
+          </div>
+          <div className="movieCategGroup">
+            <h3 className="movieCategTag">Actors:</h3>
+            <h3>{movie.Actors}</h3>
+          </div>
+        </div>
+        <div className="movieCategColumn">
+          <div className="movieCategGroup">
+            <h3 className="movieCategTag">Genres:</h3>
+            <h3>{movie.Genre}</h3>
+          </div>
+          <div className="movieCategGroup">
+            <h3 className="movieCategTag">Plot:</h3>
+            <h3>{movie.Plot}</h3>
+          </div>
+          <div className="movieCategGroup">
+            <h3 className="movieCategTag">Ratings:</h3>
+            <h3>{movie.imdbRating}⭐</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
