d9bd77cc1a9a7b7f5f62cdf239ce40ae4a320df6
RaresButuc <eusuntbutuc01@gmail.com>
2023-10-24 23:17:43 +0300

    Rewriting the Movie Stats Page

--- client/src/components/HomeMovie.js
+++ client/src/components/HomeMovie.js
@@ -1,35 +1,29 @@
 export default function ChosenMovieInformations({ movie }) {
   return (
     <div className="searchedMovie">
-            <h1>{movie.Title}</h1>
-
+      <h1>{movie.Title}</h1>
       <img className="myimg" src={movie.Poster} alt="movie.poster.png" />
       <br></br>
-      <h3>
-        Release Date:<br></br>
-        {movie.Released}
-      </h3>
-      <h3>
-        Directed by:<br></br> {movie.Director}
-      </h3>
-      <h3>
-        Screenwriting by:<br></br>
-        {movie.Writer}
-      </h3>
-      <h3>
-        Actors:<br></br>
-        {movie.Actors}
-      </h3>
-      <h3>
-        Genres: <br></br>
-        {movie.Genre}
-      </h3>
-      <h3>
-        Plot: <br></br>
-        {movie.Plot}
-      </h3>
-      <h3>Ratings: <br></br>{movie.imdbRating}</h3>
-      
+      <h3 className="movieCategTag">Release Date:</h3>
+      <h3>{movie.Released}</h3>
+
+      <h3 className="movieCategTag">Directed by:</h3>
+      <h3>{movie.Director}</h3>
+
+      <h3 className="movieCategTag">Screenwriting by:</h3>
+      <h3>{movie.Writer}</h3>
+
+      <h3 className="movieCategTag">Actors:</h3>
+      <h3>{movie.Actors}</h3>
+
+      <h3 className="movieCategTag">Genres: </h3>
+      <h3>{movie.Genre}</h3>
+
+      <h3 className="movieCategTag">Plot:</h3>
+      <h3>{movie.Plot}</h3>
+
+      <h3 className="movieCategTag">Ratings: </h3>
+      <h3>{movie.imdbRating}⭐</h3>
     </div>
   );
 }
